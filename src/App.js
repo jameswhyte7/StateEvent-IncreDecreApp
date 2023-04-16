f882a16fd758baa437d7c49e7a587145afcedc65
James Whyte <boikhristmas@gmail.com>
2023-04-16 11:57:52 +0000

    finshed increment and decrement app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+
+  }
+  handleIncreaseCount = (e) => {
+    e.preventDefault();
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleDecreaseCount = (e) => {
+    e.preventDefault();
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter: {this.state.count}</h1>
+        <hr/>
+        <button onClick={this.handleIncreaseCount}>Increase</button>
+        <button onClick={this.handleDecreaseCount}>Decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
